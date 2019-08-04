3617d66a08f01fe57663ccb900231019552458c9
Harshal Bharambe <harshalbharambe@gmail.com>
2019-08-04 17:50:53 +0530

    Update Myapplication.java

--- src/main/java/com/assignment/springboot/mongo/Myapplication.java
+++ src/main/java/com/assignment/springboot/mongo/Myapplication.java
@@ -2,12 +2,14 @@ package com.assignment.springboot.mongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Main implementation class which serves two purpose in a spring boot application: Configuration and bootstrapping.
  * @author yatin-batra
  */
 @SpringBootApplication
+@EnableSwagger2
 public class Myapplication {
 
 	public static void main(String[] args) {
